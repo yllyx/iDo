3472cfbc147a4d37c1b68518b0495bf201f0a2d0
yllyx <Yunxiang.Lon@gmail.com>
2015-09-13 09:18:10 +0800

    添加登陆和注册的controller

---
+++ src/main/java/com/yllyx/ido/controller/login/CLogin.java
@@ -0,0 +1,15 @@
+package com.yllyx.ido.controller.login;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+
+public class CLogin
+{
+    @RequestMapping("login")
+    public void login()
+    {
+
+    }
+}
---
+++ src/main/java/com/yllyx/ido/controller/register/CRegister.java
@@ -0,0 +1,14 @@
+package com.yllyx.ido.controller.register;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CRegister
+{
+    @RequestMapping("register")
+    public void register()
+    {
+
+    }
+}
